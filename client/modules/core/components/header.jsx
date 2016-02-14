943ade3b28e58ba16b45930853504d4c12f6c948
Sung Won Cho <mikeswcho@gmail.com>
2016-02-14 14:09:00 +1100

    Link blog in the header

--- client/modules/core/components/header.jsx
+++ client/modules/core/components/header.jsx
@@ -2,11 +2,21 @@ import React from 'react';
 import UserActions from './header.user_actions.jsx';
 
 let Header =  React.createClass({
-  renderUserActions() {
+  renderMenus() {
     if (this.props.currentUser) {
       return (
         <UserActions currentUser={this.props.currentUser}
                      handleLogout={this.props.logout} />
+      );
+    } else {
+      return (
+        <ul className="nav navbar-nav pull-xs-right">
+          <li className="nav-item">
+            <a className="nav-link" href="http://blog.coddee.io">
+              Blog
+            </a>
+          </li>
+        </ul>
       );
     }
   },
@@ -21,9 +31,14 @@ let Header =  React.createClass({
 
   render() {
     return (
-      <nav className="navbar navbar-dark bg-inverse">
-        <a className="navbar-brand" href={this.getRootPath()}>Coddee</a>
-        {this.renderUserActions()}
+      <nav className="navbar navbar-dark">
+        <div className="container">
+          <a className="navbar-brand" href={this.getRootPath()}>
+            Coddee
+          </a>
+
+          {this.renderMenus()}
+        </div>
       </nav>
     );
   }
