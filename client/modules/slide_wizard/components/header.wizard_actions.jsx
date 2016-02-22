b9850bab6d35cc7b9285b413fc48aaeb6f6911db
Sung Won Cho <mikeswcho@gmail.com>
2016-02-22 13:14:45 +1100

    Disable remove slide button if there is no current slide

--- client/modules/slide_wizard/components/header.wizard_actions.jsx
+++ client/modules/slide_wizard/components/header.wizard_actions.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import classNames from 'classnames';
 
 export default React.createClass({
   render() {
+    let removeActionClass = classNames({
+      'nav-link': true,
+      disabled: !this.canRemoveSlide()
+    });
+
     return (
       <div className="nav navbar-nav wizard-actions">
         <li className="nav-item wizard-action">
@@ -10,12 +16,16 @@ export default React.createClass({
           </a>
         </li>
         <li className="nav-item wizard-action">
-          <a href="#" className="nav-link" onClick={this.removeSlide}>
+          <a href="#" className={removeActionClass} onClick={this.removeSlide}>
             <i className="fa fa-minus"></i>
           </a>
         </li>
       </div>
     );
+  },
+
+  canRemoveSlide() {
+    return !! this.props.currentSlideNumber;
   },
 
   addSlide(e) {
@@ -27,6 +37,10 @@ export default React.createClass({
 
   removeSlide(e) {
     e.preventDefault();
+    if (!this.canRemoveSlide()) {
+      return;
+    }
+
     const {removeSlide, slideDeck, currentSlideNumber} = this.props;
 
     removeSlide(slideDeck._id, currentSlideNumber);
