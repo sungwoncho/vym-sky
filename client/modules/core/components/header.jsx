9e1be01ee72d6a2da668b2bb440d0c8f7695c3e6
Sung Won Cho <mikeswcho@gmail.com>
2016-03-09 10:08:02 +1100

    Refactor header

--- client/modules/core/components/header.jsx
+++ client/modules/core/components/header.jsx
@@ -1,47 +1,68 @@
 import React from 'react';
-import UserActions from './header.user_actions.jsx';
 
-let Header =  React.createClass({
-  renderMenus() {
-    if (this.props.currentUser) {
-      return (
-        <UserActions currentUser={this.props.currentUser}
-                     handleLogout={this.props.logout} />
-      );
-    } else {
-      return (
-        <ul className="nav navbar-nav pull-xs-right">
-          <li className="nav-item">
-            <a className="nav-link" href="http://blog.vym.io">
-              Blog
-            </a>
-          </li>
-        </ul>
-      );
-    }
-  },
-
-  getRootPath() {
-    if (this.props.currentUser) {
+const Header = ({currentUser, logout}) => {
+  function getRootPath() {
+    if (currentUser) {
       return '/home';
     } else {
       return '/';
     }
-  },
+  }
 
-  render() {
+  return (
+    <nav className="navbar navbar-dark">
+      <div className="container">
+        <a className="navbar-brand" href={getRootPath()}>
+          vym
+        </a>
+
+        <Menus currentUser={currentUser}
+          logout={logout} />
+      </div>
+    </nav>
+  );
+};
+
+
+const Menus = ({currentUser, logout}) => {
+  if (currentUser) {
     return (
-      <nav className="navbar navbar-dark">
-        <div className="container">
-          <a className="navbar-brand" href={this.getRootPath()}>
-            vym
+      <UserActions currentUser={currentUser}
+                   handleLogout={logout} />
+    );
+  } else {
+    return (
+      <ul className="nav navbar-nav pull-xs-right">
+        <li className="nav-item">
+          <a className="nav-link" href="http://blog.vym.io">
+            Blog
           </a>
-
-          {this.renderMenus()}
-        </div>
-      </nav>
+        </li>
+      </ul>
     );
   }
-});
+};
+
+const UserActions = ({currentUser, handleLogout}) => {
+  function onLogout(e) {
+    e.preventDefault();
+    handleLogout();
+  }
+
+  return (
+    <ul className="nav navbar-nav pull-xs-right">
+      <div className="nav-item dropdown">
+        <a href="#" className="nav-link dropdown-toggle" data-toggle="dropdown">
+          {currentUser.profile.name}
+        </a>
+        <div className="dropdown-menu">
+          <a className="dropdown-item" href="#" onClick={onLogout}>
+            Logout
+          </a>
+        </div>
+      </div>
+    </ul>
+  );
+};
 
 export default Header;
