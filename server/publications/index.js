62ef536ffe6c15d25cb1f43738c91bccec7b05d1
Sung Won Cho <mikeswcho@gmail.com>
2016-03-19 10:16:52 +1100

    Delete unnecessary lines and files

--- server/publications/index.js
+++ server/publications/index.js
@@ -1,11 +1,9 @@
 import slideDecks from './slide_decks';
 import repos from './repos';
-import pullRequests from './pull_requests';
 import users from './users';
 
 export default function () {
   slideDecks();
   repos();
-  pullRequests();
   users();
 }
