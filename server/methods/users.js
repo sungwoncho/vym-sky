24ce5e630d7c55349e3edf818679704cf7ed34c3
Sung Won Cho <mikeswcho@gmail.com>
2016-04-01 17:38:54 +1100

    Increase plan quantity when already subbed

--- server/methods/users.js
+++ server/methods/users.js
@@ -57,13 +57,38 @@ export default function () {
 
     'users.createOrUpdateSubscription'(token) {
       let stripe = stripeAPI(Meteor.settings.stripeSecretKey);
-      let customer = Meteor.wrapAsync(stripe.customers.create, stripe.customers)({
-        source: token.id,
-        plan: 'pro',
-        email: token.email
-      });
+      let currentUser = Meteor.users.findOne(this.userId);
 
-      return customer;
+      if (currentUser.stripeCustomerId) {
+        let sub = Meteor.wrapAsync(stripe.customers.retrieveSubscription, stripe.customers)(
+          currentUser.stripeCustomerId,
+          currentUser.stripeSubscriptionId
+        );
+
+        Meteor.wrapAsync(stripe.customers.updateSubscription, stripe.customers)(
+          currentUser.stripeCustomerId,
+          currentUser.stripeSubscriptionId,
+          {
+            quantity: sub.quantity + 1
+          }
+        );
+
+      } else {
+        let customer = Meteor.wrapAsync(stripe.customers.create, stripe.customers)({
+          source: token.id,
+          plan: 'pro',
+          email: token.email
+        });
+        let sub = customer.subscriptions.data[0];
+
+        Meteor.users.update(this.userId, {
+          $set: {
+            stripeCustomerId: customer.id,
+            stripeSubscriptionId: sub.id
+          }
+        });
+      }
+
     }
 
   });
