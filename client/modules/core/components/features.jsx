b64e7754c415e8c55bd40004249007e149e23993
Sung Won Cho <mikeswcho@gmail.com>
2016-04-01 14:11:17 +1100

    Mark upcoming features

--- client/modules/core/components/features.jsx
+++ client/modules/core/components/features.jsx
@@ -6,9 +6,9 @@ const Features = () => (
       <h2 className="heading">Review code better</h2>
       <div className="sub-heading text-muted">Get across your idea fast and efficiently</div>
     </div>
-    <div className="container feature">
+    <div className="container features">
       <div className="row">
-        <div className="col-xs-12 col-lg-4">
+        <div className="col-xs-12 col-lg-4 feature">
           <div className="feature-heading">
             <i className="feature-icon fa fa-clock-o"></i> Real time
           </div>
@@ -17,7 +17,7 @@ const Features = () => (
             the browser. No need to share screen.
           </p>
         </div>
-        <div className="col-xs-12 col-lg-4">
+        <div className="col-xs-12 col-lg-4 feature">
           <div className="feature-heading">
             <i className="feature-icon fa fa-scissors"></i> Organize
           </div>
@@ -26,9 +26,9 @@ const Features = () => (
             code in Pull Requests.
           </p>
         </div>
-        <div className="col-xs-12 col-lg-4">
+        <div className="col-xs-12 col-lg-4 feature upcoming">
           <div className="feature-heading">
-            <span className="feature-icon octicon octicon-git-merge"></span> Vote and merge
+            <span className="feature-icon octicon octicon-git-merge"></span> Vote and merge (In development)
           </div>
           <p className="feature-desc">
             At the end of the presentation, cast a vote and merge automatically.
@@ -36,7 +36,7 @@ const Features = () => (
         </div>
       </div>
       <div className="row">
-        <div className="col-xs-12 col-lg-4">
+        <div className="col-xs-12 col-lg-4 feature">
           <div className="feature-heading">
             <i className="feature-icon fa fa-github"></i> GitHub integration
           </div>
@@ -44,16 +44,16 @@ const Features = () => (
             Just login with GitHub and work with your data.
           </p>
         </div>
-        <div className="col-xs-12 col-lg-4">
+        <div className="col-xs-12 col-lg-4 feature upcoming">
           <div className="feature-heading">
-            <span className="feature-icon octicon octicon-dashboard"></span> View coverage
+            <span className="feature-icon octicon octicon-dashboard"></span> View coverage (In development)
           </div>
           <p className="feature-desc">
             See how much of the Pull Request is covered by the slide. You can be
             confident that you understand the Pull Request after following it.
           </p>
         </div>
-        <div className="col-xs-12 col-lg-4">
+        <div className="col-xs-12 col-lg-4 feature">
           <div className="feature-heading">
             <span className="feature-icon octicon octicon-lock"></span> Your code is safe
           </div>
